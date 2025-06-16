0cba54f54b7009eb30ff93433a0204810c16db72
DaniSanchez01 <daniel.sanchez.rodriguez@feverup.com>
2025-06-16 23:29:50 +0200

    use if/else

--- src/app/navbar/navbar.component.tsx
+++ src/app/navbar/navbar.component.tsx
@@ -1,10 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import DesktopNavbar from './DesktopNavbar';
 import MobileNavbar from './MobileNavbar';
 
 import './navbar.css';
 
 function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768); // md breakpoint
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
   return (
     <nav className="navbar navbar-light cst-navbar sticky-top">
       <Link className="navbar-brand cst-fit-height" to="/">
@@ -15,12 +28,7 @@ function Navbar() {
         />
       </Link>
 
-      <div className="d-none d-md-flex">
-        <DesktopNavbar />
-      </div>
-      <div className="d-md-none">
-        <MobileNavbar />
-      </div>
+      {isMobile ? <MobileNavbar /> : <DesktopNavbar />}
     </nav>
   );
 }
