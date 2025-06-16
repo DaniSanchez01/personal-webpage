e5ca58e98ef015be7b02744c80fa7bd52a0f6136
DaniSanchez01 <daniel.sanchez.rodriguez@feverup.com>
2025-06-16 23:02:25 +0200

    mvp

--- src/app/navbar/navbar.component.tsx
+++ src/app/navbar/navbar.component.tsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+import './navbar.css';
 
 function Navbar() {
+  const [menuIsClosed, setMenuIsClosed] = useState(true);
+
   return (
     <nav className="navbar navbar-light cst-navbar sticky-top">
       <Link className="navbar-brand cst-fit-height" to="/">
@@ -13,7 +19,15 @@ function Navbar() {
         />
       </Link>
 
-      <div className="cst-internal-links">
+      <button
+        className="btn d-md-none cst-hamburger-btn"
+        type="button"
+        onClick={() => setMenuIsClosed(!menuIsClosed)}
+      >
+        <FontAwesomeIcon icon={menuIsClosed ? faBars : faTimes} size="lg" />
+      </button>
+
+      <div className="cst-internal-links d-none d-md-flex">
         <Link
           to="/"
           className="nav-link cst-nav-link cst-underline-link cst-color-change-link"
@@ -46,7 +60,7 @@ function Navbar() {
         </Link>
       </div>
 
-      <div className="cst-external-links">
+      <div className="cst-external-links d-none d-md-flex">
         <a
           href="https://www.linkedin.com/in/daniel-sánchez-rodríguez-629626255/"
           target="_blank"
@@ -64,6 +78,71 @@ function Navbar() {
           <FontAwesomeIcon icon={faGithub} size="lg" />
         </a>
       </div>
+
+      {!menuIsClosed && (
+        <div className="cst-mobile-menu d-md-none">
+          <div className="cst-mobile-menu-content">
+            <Link
+              to="/"
+              className="cst-mobile-link"
+              onClick={() => setMenuIsClosed(true)}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="cst-mobile-link"
+              onClick={() => setMenuIsClosed(true)}
+            >
+              About Me
+            </Link>
+            <Link
+              to="/projects"
+              className="cst-mobile-link"
+              onClick={() => setMenuIsClosed(true)}
+            >
+              Projects
+            </Link>
+            <Link
+              to="/blog"
+              className="cst-mobile-link"
+              onClick={() => setMenuIsClosed(true)}
+            >
+              Blog
+            </Link>
+            <Link
+              to="/contact"
+              className="cst-mobile-link"
+              onClick={() => setMenuIsClosed(true)}
+            >
+              Contact
+            </Link>
+
+            <hr className="cst-mobile-divider" />
+
+            <div className="cst-mobile-social">
+              <a
+                href="https://www.linkedin.com/in/daniel-sánchez-rodríguez-629626255/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cst-mobile-social-link"
+                onClick={() => setMenuIsClosed(true)}
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="lg" />
+              </a>
+              <a
+                href="https://github.com/DaniSanchez01"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cst-mobile-social-link"
+                onClick={() => setMenuIsClosed(true)}
+              >
+                <FontAwesomeIcon icon={faGithub} size="lg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
